27b45133b96bbdf83bb2f517f0775b4985b28fa2
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2022-10-28 20:47:23 +0530

    Create Weather Observation Station 10.sql

---
+++ Weather Observation Station 10.sql
@@ -0,0 +1,5 @@
+# Question : Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+
+# Answer :
+
+SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('A','E','I','O','U') AND LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');    
