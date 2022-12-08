f8a7295f466a03f36bfd04c51801dd39c3373ffe
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2022-12-08 08:48:46 +0530

    Create Occupations.sql

---
+++ Occupations.sql
@@ -0,0 +1,28 @@
+# Question :  Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation.
+The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+# Answer :  
+
+select
+    Doctor,
+    Professor,
+    Singer,
+    Actor
+from (
+    select
+        NameOrder,
+        max(case Occupation when 'Doctor' then Name end) as Doctor,
+        max(case Occupation when 'Professor' then Name end) as Professor,
+        max(case Occupation when 'Singer' then Name end) as Singer,
+        max(case Occupation when 'Actor' then Name end) as Actor
+    from (
+            select
+                Occupation,
+                Name,
+                row_number() over(partition by Occupation order by Name ASC) as NameOrder
+            from Occupations
+         ) as NameLists
+    group by NameOrder
+    ) as Names
