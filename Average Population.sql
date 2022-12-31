92375dd171a01038f10bb63b9fa5d13d68ef9748
Sakshi Milind Gome <112328520+sakshi-0009@users.noreply.github.com>
2022-12-31 23:25:31 +0530

    Update Average Population.sql

--- Average Population.sql
+++ Average Population.sql
@@ -1,8 +1,7 @@
-/*
- Question : Query the average population for all cities in CITY, rounded down to the nearest integer.
- */
+
+# Question : Query the average population for all cities in CITY, rounded down to the nearest integer.
  
- Answer :
+# Answer :
  
 SELECT ROUND(AVG(POPULATION), 0)
 FROM CITY;
